1f20b7958509a3aa834d607bcdb57aec3043879f
Charran Thangeswaran <charranthangeswaran@gmail.com>
2025-12-01 10:55:52 -0500

    Add read and respond message function

--- bot.py
+++ bot.py
@@ -1,8 +1,15 @@
 import discord
 
 class MyClient(discord.Client):
-  async def on_ready(self):
-    print('Logged on as {0}!'.format(self.user))
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+            
+        if message.content.startswith('$hello'):
+            await message.channel.send('Hello World!') # Respond with 'Hello World!' when $hello is invoked
 
 intents = discord.Intents.default()
 intents.message_content = True
